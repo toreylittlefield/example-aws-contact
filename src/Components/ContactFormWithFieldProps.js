fdba28a4eafdda772b278941a5440a96e2b4f7a9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-30 04:32:40 -0400

    using field props for most inputs

--- src/Components/ContactFormWithFieldProps.js
+++ src/Components/ContactFormWithFieldProps.js
@@ -36,34 +36,13 @@ const ContactFormWithFieldProps = () => {
   return (
     <form className="contact-form" onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
+      <input id="firstName" type="text" {...formik.getFieldProps('firstName')} />
       {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
       <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
+      <input id="lastName" type="text" {...formik.getFieldProps('lastName')} />
       {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
       <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
+      <input id="email" type="email" {...formik.getFieldProps('email')} />
       {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <ReCAPTCHA
         style={{ display: 'inline-block' }}

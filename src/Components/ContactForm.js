9469fc6d4957fb01bc894ef1cb6e68bd59ee6562
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-29 21:58:14 -0400

    formik example 2

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { useFormik } from 'formik';
 
 const ContactForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
   const formik = useFormik({
     initialValues: {
+      firstName: '',
+      lastName: '',
       email: '',
     },
     onSubmit: (values) => {
@@ -14,6 +18,24 @@ const ContactForm = () => {
   });
   return (
     <form className="contact-form" onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
       <label htmlFor="email">Email Address</label>
       <input
         id="email"

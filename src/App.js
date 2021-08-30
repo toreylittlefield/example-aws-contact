66732968086992ea6cf170527e2b34e52171289a
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-30 04:32:22 -0400

    add example sections

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import ContactFormWithFieldProps from './Components/ContactFormWithFieldProps';
 
 const App = () => (
   <div className="App">
-    <ContactForm />
-    <ContactFormWithFieldProps />
+    <section>
+      <h2>Formik Example With Yup</h2>
+      <p>All the boilerplate stuff</p>
+      <ContactForm />
+    </section>
+    <section>
+      <h2>Formik With Field Props</h2>
+      <p>Reduces boilerplate</p>
+      <ContactFormWithFieldProps />
+    </section>
   </div>
 );
 

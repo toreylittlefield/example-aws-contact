3417b6ddd55e23e27b4a4b928c1c854499c2ded0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-31 10:35:51 -0400

    add MUI collapsing card for code block

--- src/Components/ReactCodeBlock.js
+++ src/Components/ReactCodeBlock.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism/';
 import * as beautify from 'js-beautify';
-import { IconButton, Tooltip } from '@material-ui/core';
-import { FileCopy } from '@material-ui/icons';
+import { IconButton, Tooltip, Collapse, Card, CardActions, CardContent } from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+import { FileCopy, ExpandMoreTwoTone, ExpandLessTwoTone } from '@material-ui/icons';
 
 const options = {
   indent_size: '1',
@@ -25,32 +27,55 @@ const options = {
 };
 
 const ReactCodeBlock = ({ code }) => {
+  const [open, setOpen] = useState(false);
   const handleCopy = () => {
     window.navigator.clipboard.writeText(code);
   };
+  const handleOpen = () => setOpen((prev) => !prev);
   return (
-    <div style={{ position: 'relative', display: 'grid', placeContent: 'flex-start' }}>
-      <Tooltip title="Copy Snippet" style={{ position: 'absolute', right: 0, top: 0, color: 'white' }}>
-        <IconButton onClick={handleCopy}>
-          <FileCopy />
-        </IconButton>
-      </Tooltip>
-      <SyntaxHighlighter
-        language="jsx"
-        showLineNumbers={true}
-        showInlineLineNumbers={true} // <-- add this prop!
-        wrapLines={true}
-        customStyle={{
-          margin: 0,
-          padding: '0.25em',
-          wordBreak: 'break-all',
-          whiteSpace: 'pre-wrap',
-          boxShadow: '0px 2px 4px rgba(50,50,93,.1)',
-        }}
-        style={darcula}
-        children={beautify(code, options)}
-      />
-    </div>
+    <Card>
+      <CardContent>
+        <CardActions disableSpacing style={{ padding: '0', position: 'relative' }}>
+          <div style={{ display: 'flex', position: 'absolute', right: 0, top: 0, padding: '1em 2em' }}>
+            <Tooltip title="Copy Snippet" style={{ color: 'white' }}>
+              <IconButton onClick={handleCopy}>
+                <FileCopy />
+              </IconButton>
+            </Tooltip>
+            <Tooltip
+              color={open ? 'secondary' : ''}
+              style={!open ? { color: green[500] } : {}}
+              title={open ? `Collapse Snippet` : `Expand Snippet`}
+            >
+              <IconButton>
+                {open ? (
+                  <ExpandLessTwoTone color={'secondary'} onClick={handleOpen} />
+                ) : (
+                  <ExpandMoreTwoTone style={{ color: green[500] }} onClick={handleOpen} />
+                )}
+              </IconButton>
+            </Tooltip>
+          </div>
+        </CardActions>
+        <Collapse collapsedSize="200px" in={open} timeout="auto" style={open ? '' : { overflowY: 'scroll' }}>
+          <SyntaxHighlighter
+            language="jsx"
+            showLineNumbers={true}
+            showInlineLineNumbers={true} // <-- add this prop!
+            wrapLines={true}
+            customStyle={{
+              margin: 0,
+              padding: '0.25em',
+              wordBreak: 'break-all',
+              whiteSpace: 'pre-wrap',
+              boxShadow: '0px 2px 4px rgba(50,50,93,.1)',
+            }}
+            style={darcula}
+            children={beautify(code, options)}
+          />
+        </Collapse>
+      </CardContent>
+    </Card>
   );
 };
 

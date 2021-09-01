2e3d724e4a730b68570bc8d77b8e94e61ec6a1d4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-01 04:13:40 -0400

    create a section component to reuse

---
+++ src/Components/Section.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ReactCodeBlock } from '.';
+
+export const Section = ({
+  sectionTitle = '',
+  sectionDescription = '',
+  ComponentToRender = {},
+  codeString = '',
+}) => {
+  return (
+    <section>
+      <div className="container">
+        <h2>{sectionTitle}</h2>
+        <p>{sectionDescription}</p>
+        <ComponentToRender />
+      </div>
+      <ReactCodeBlock code={codeString} />
+    </section>
+  );
+};

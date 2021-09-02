564befe620833579b6e7f5a55cd0644b05b79ba2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-01 23:39:35 -0400

    add pages Example 4 & 5

---
+++ src/Pages/ExampleFive.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ContactFormWithUseFieldHookMui,
+  contactFormWithUseFieldHookMuiCodeString,
+} from '../Components';
+import { Section } from '../Components/Section';
+
+const ExampleFive = () => (
+  <Section
+    sectionTitle="Formik useField Hook Example Extended With MUI Components"
+    sectionDescription="Again extending the useField example and adding MUI components using the 'AS' prop from the formik Field Component"
+    ComponentToRender={ContactFormWithUseFieldHookMui}
+    codeString={contactFormWithUseFieldHookMuiCodeString}
+  />
+);
+
+export default ExampleFive;
---
+++ src/Pages/ExampleFour.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ContactFormWithUseFieldHook,
+  ContactFormWithUseFieldHookCodeString,
+} from '../Components';
+import { Section } from '../Components/Section';
+
+const ExampleFour = () => (
+  <Section
+    sectionTitle="Formik useField Hook Example"
+    sectionDescription="Uses formik custom useField hook for abstraction and allows us to create custom field components"
+    ComponentToRender={ContactFormWithUseFieldHook}
+    codeString={ContactFormWithUseFieldHookCodeString}
+  />
+);
+
+export default ExampleFour;

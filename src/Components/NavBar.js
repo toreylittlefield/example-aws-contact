1c3415e7cc8e1676a689b603d16e9b6fcd9dabd7
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-01 23:39:17 -0400

    add location value to Tabs

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,11 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AppBar, Tabs, Tab } from '@material-ui/core';
 import { amber, common } from '@material-ui/core/colors';
 
 export const NavBar = () => {
+  const location = useLocation();
   return (
     <AppBar
       position="static"
@@ -12,7 +13,7 @@ export const NavBar = () => {
     >
       <Tabs
         variant="fullWidth"
-        // // value={value}
+        value={location.pathname}
         // onChange={handleChange}
         aria-label="nav tabs example"
       >
@@ -33,6 +34,18 @@ export const NavBar = () => {
           value="/ExampleThree"
           component={Link}
           label="Example 3: Formik Context"
+        />
+        <Tab
+          to="/ExampleFour"
+          value="/ExampleFour"
+          component={Link}
+          label="Example 4: useField Hook"
+        />
+        <Tab
+          to="/ExampleFive"
+          value="/ExampleFive"
+          component={Link}
+          label="Example 5: Material UI"
         />
       </Tabs>
     </AppBar>

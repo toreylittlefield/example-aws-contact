7b94bc33da2403599ce8895832b51afa1c6a607e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-01 23:38:51 -0400

    refactor and add page routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 /* eslint-disable no-unused-vars */
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import {
-  NavBar,
-  ContactFormReactContext,
-  contactFormReactContextCodeString,
-  ContactFormWithUseFieldHook,
-  ContactFormWithUseFieldHookCodeString,
-  ContactFormWithUseFieldHookMui,
-  contactFormWithUseFieldHookMuiCodeString,
-} from './Components';
+import { NavBar } from './Components';
 
-import { Section } from './Components/Section';
-import { ExampleOne, ExampleTwo, ExampleThree } from './Pages';
+import {
+  ExampleOne,
+  ExampleTwo,
+  ExampleThree,
+  ExampleFour,
+  ExampleFive,
+} from './Pages';
 
 const App = () => (
   <Router>
@@ -28,25 +25,12 @@ const App = () => (
         <Route path="/ExampleThree">
           <ExampleThree />
         </Route>
-
-        <Section
-          sectionTitle="Formik React Context"
-          sectionDescription="Formik basically uses the React Context which even further abstracts"
-          ComponentToRender={ContactFormReactContext}
-          codeString={contactFormReactContextCodeString}
-        />
-        <Section
-          sectionTitle="Formik useField Hook Example"
-          sectionDescription="Uses formik custom useField hook for abstraction and allows us to create custom field components"
-          ComponentToRender={ContactFormWithUseFieldHook}
-          codeString={ContactFormWithUseFieldHookCodeString}
-        />
-        <Section
-          sectionTitle="Formik useField Hook Example Extended With MUI Components"
-          sectionDescription="Again extending the useField example and adding MUI components using the 'AS' prop from the formik Field Component"
-          ComponentToRender={ContactFormWithUseFieldHookMui}
-          codeString={contactFormWithUseFieldHookMuiCodeString}
-        />
+        <Route path="/ExampleFour">
+          <ExampleFour />
+        </Route>
+        <Route path="/ExampleFive">
+          <ExampleFive />
+        </Route>
       </Switch>
     </div>
   </Router>

c69825b4a4f5ca5d9ed680a5ac06d5282e167c39
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-04 10:47:10 -0400

    remove switch component from react-router, working on gsap timeline animation but buggy

--- src/App.js
+++ src/App.js
@@ -26,22 +26,38 @@ const routes = [
   {
     path: '/ExampleOne',
     component: ExampleOne,
+    key: 'ExampleOne',
+    exact: true,
   },
   {
     path: '/ExampleTwo',
     component: ExampleTwo,
+    key: 'ExampleTwo',
+    exact: true,
   },
   {
     path: '/ExampleThree',
     component: ExampleThree,
+    key: 'ExampleThree',
+    exact: true,
   },
   {
     path: '/ExampleFour',
     component: ExampleFour,
+    key: 'ExampleFour',
+    exact: true,
   },
   {
     path: '/ExampleFive',
     component: ExampleFive,
+    key: 'ExampleFive',
+    exact: true,
+  },
+  {
+    path: '*',
+    component: Redirect,
+    key: 'router-redirect-key',
+    exact: false,
   },
 ];
 
@@ -50,7 +66,7 @@ const routes = [
  */
 const initGsapTiming = {
   duration: 0.6,
-  stagger: 0.3,
+  stagger: 0.6,
   totalTime: 0,
 };
 
@@ -62,7 +78,12 @@ const App = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('sm'));
   const matchesMemo = useMemo(() => matches, [matches]);
-  const transitionRef = useRef(null);
+  const exitRef = useRef(null);
+  const tl = useRef(null);
+
+  useEffect(() => {
+    tl.current = gsap.timeline();
+  }, []);
 
   const initialState = () => {
     const pathOnLoad = history.location.pathname;
@@ -78,6 +99,13 @@ const App = () => {
     if (history.action === 'POP') {
       setviewIndex(initialState);
     }
+    document
+      .querySelectorAll('iframe[title="recaptcha challenge"]', '.page')
+      ?.forEach((element, index) => {
+        if (index === 0) return;
+        if (element.tagName === 'IFRAME')
+          return element.parentElement.parentElement.remove();
+      });
   }, [location]);
 
   const handleChange = (event) => {
@@ -98,16 +126,23 @@ const App = () => {
 
   const onEnter = (node) => {
     console.log('enter', gsapTimingState);
-    console.log({ refExit: transitionRef.current });
+    console.log({ refEnter: exitRef.current });
     if (!node) return;
+    // if (tl.current && tl.current.isActive()) return;
+
+    // gsap.killTweensOf(node);
     gsap.set(node, { display: 'none' });
     const [firstChild, secondChild] = node.children;
+    gsap.set(document.body, { overflow: 'hidden' });
 
-    gsap.from([firstChild.children, secondChild, secondChild.children], {
+    tl.current.from([firstChild.children, secondChild, secondChild.children], {
       onStart: () => gsap.set(node, { clearProps: 'display' }),
-      onComplete: () => gsap.set(document.body, { clearProps: 'overflow' }),
+      onComplete: () => {
+        gsap.set(document.body, { clearProps: 'overflow' });
+        tl.current.totalProgress(1).clear();
+      },
       ease: 'power3.Out',
-      delay: gsapTimingState.totalTime + 0.05,
+      // delay: gsapTimingState.totalTime + 0.05,
       autoAlpha: 0,
       [moveGsap]: 10,
       stagger: {
@@ -118,27 +153,28 @@ const App = () => {
   };
 
   const onExit = (node) => {
-    console.log({ refExit: transitionRef.current });
+    console.log({ refExit: exitRef.current, tl });
     if (!node) return;
+    if (tl.current && tl.current.isActive()) return;
+    // gsap.killTweensOf(node);
+    const tline = gsap.timeline();
+    // tline.autoRemoveChildren();
+
     const [firstChild, secondChild] = node.children;
     gsap.set(document.body, { overflow: 'hidden' });
-    const exitAnimation = gsap.to(
-      [firstChild.children, secondChild, secondChild.children],
-      {
-        onStart: () => setTransitionIn(false),
-        onComplete: () => {
-          setTransitionIn(true);
-          // gsap.set(node, { display: 'none' });
-        },
-        ease: 'power3.In',
-        duration: gsapTimingState.duration,
-        [moveGsap]: -30,
-        autoAlpha: 0,
-        stagger: {
-          amount: gsapTimingState.stagger,
-        },
-      }
-    );
+    console.log({ matchesMemo });
+    const elementsToAnimate = !matchesMemo
+      ? [firstChild, secondChild.children]
+      : [firstChild.children, secondChild, secondChild.children];
+    const exitAnimation = tl.current.to(elementsToAnimate, {
+      ease: 'power3.In',
+      duration: gsapTimingState.duration,
+      [moveGsap]: -100,
+      autoAlpha: 0,
+      stagger: {
+        amount: gsapTimingState.stagger,
+      },
+    });
     // set the totalTime duration of the animation
     if (gsapTimingState.totalTime === 0) {
       setGsapTimingState((prev) => {
@@ -148,30 +184,46 @@ const App = () => {
       });
     }
   };
+
   return (
     <div className="App">
       <NavBar handleChange={handleChange} />
-      <TransitionGroup>
-        <Transition
+      {/* <TransitionGroup> */}
+      {/* 
+      <Transition
           in={transitionIn}
-          timeout={gsapTimingState.totalTime + 200}
+          timeout={gsapTimingState.totalTime + 0.02}
           onExit={onExit}
           onEntering={onEnter}
           unmountOnExit
-          ref={transitionRef}
+          ref={exitRef}
           key={location.key}
         >
-          <Switch location={location}>
-            {routes.map(({ path, component: Component }) => (
-              <Route key={path} exact path={path} component={Component} />
-            ))}
-
-            <Route path="*">
-              <Redirect to="/ExampleOne" />
-            </Route>
-          </Switch>
+          <Switch location={location}> */}
+      {routes.map(({ path, component: Component, key, exact }) => (
+        <Route key={key} path={path} exact>
+          {
+            ({ match }) => (
+              // match !== null ? (
+              <Transition
+                in={match !== null}
+                timeout={gsapTimingState.totalTime * 950 ?? 0}
+                onExit={onExit}
+                onEntering={onEnter}
+                unmountOnExit
+                ref={exitRef}
+                // key={location.key
+              >
+                <Component to={path === '*' ? '/ExampleOne' : null} />
+              </Transition>
+            )
+            // ) : null
+          }
+        </Route>
+      ))}
+      {/* </Switch>
         </Transition>
-      </TransitionGroup>
+      </TransitionGroup> */}
     </div>
   );
 };
